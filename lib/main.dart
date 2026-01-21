07ae31df232ffd937f68ed176e4cb0dbdda36add
Nikhil Kumawat <nkumawat8956@gmail.com>
2026-01-21 21:53:07 +0530

    werwr

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'views/onboarding_screen.dart';
+import 'views/home_screen.dart';
+import 'services/auth_service.dart';
 import 'theme/app_theme.dart';
 
 void main() {
@@ -12,10 +14,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Management App',
+      title: 'TaskFlow',
       theme: AppTheme.darkTheme,
-      home: const OnboardingScreen(),
+      home: const AuthWrapper(),
       debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+/// Checks if user is already logged in and routes accordingly
+class AuthWrapper extends StatefulWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  bool _isLoading = true;
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final authService = AuthService();
+    final token = await authService.getToken();
+
+    setState(() {
+      _isLoggedIn = token != null && token.isNotEmpty;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator(color: Colors.white)),
+      );
+    }
+
+    return _isLoggedIn ? const HomeScreen() : const OnboardingScreen();
   }
 }
