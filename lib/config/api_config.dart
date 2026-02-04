01a6145cc537d7d50c7e90d102d2872de0784e5b
Nikhil Kumawat <nkumawat8956@gmail.com>
2026-02-04 23:03:43 +0530

    qrq2

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,7 @@
 class ApiConfig {
   // User-provided DevTunnel URL
   //static const String baseUrl = 'https://qjhcp0ph-3004.inc1.devtunnels.ms';
-  static const String baseUrl = 'https://express-js-on-vercel-silk-six-89.vercel.app';
+  //static const String baseUrl = 'https://express-js-on-vercel-silk-six-89.vercel.app';
+  static const String baseUrl =
+      'http://127.0.0.1:3004'; // Use 10.0.2.2 for Android emulator
 }
